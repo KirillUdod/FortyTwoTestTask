2379d45306892fb2d5f3a4e2007867451728c338
KirillUdod <kirilludod@gmail.com>
2017-08-28 11:48:33 +0200

    t1 task 5-6

--- apps/hello/models.py
+++ apps/hello/models.py
@@ -1,1 +1,12 @@
+from django.db import models
+
 # Create your models here.
+class MainInfo(models.Model):
+    name = models.CharField(u'Name', max_length=50)
+    last_name = models.CharField(u'Last Name', max_length=50)
+    birthday = models.DateField(u'Birthday')
+    email = models.EmailField(u'Email')
+    jabber = models.CharField(u'Jabber', max_length=50)
+    skype = models.CharField(u'Skype', max_length=50)
+    bio = models.TextField('Bio')
+    other_info = models.TextField('Other contacts', blank=True, null=True)
--- apps/hello/tests.py
+++ apps/hello/tests.py
@@ -1,6 +1,9 @@
 from django.core.urlresolvers import reverse
 from django.test import TestCase, Client
 
+import datetime
+
+from .models import MainInfo
 # Create your tests here.
 
 
@@ -11,3 +14,18 @@ class IndexTests(TestCase):
         c = Client()
         response = c.get('/')
         assert(response.content)
+
+class TestModel(TestCase):
+
+    def setUp(self):
+        MainInfo.objects.create(name='Testname', last_name='Test lastname', birthday=datetime.date.today(),
+                                email='test@test.com', jabber='test_jadder', bio='asdas')
+    def test_model(self):
+        main = MainInfo.objects.get(name="Testname")
+
+        self.assertEqual(main.last_name, 'Test lastname')
+        self.assertEqual(main.birthday, datetime.date.today())
+        self.assertEqual(main.email, 'test@test.com')
+        self.assertEqual(main.jabber, 'test_jadder')
+        self.assertEqual(main.bio, 'asdas')
+
