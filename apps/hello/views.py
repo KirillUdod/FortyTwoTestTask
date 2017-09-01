25745bb4370f5c678c5a56831865fcaa76f74c29
KirillUdod <kirilludod@gmail.com>
2017-09-01 09:31:20 +0200

    fix template

--- apps/hello/views.py
+++ apps/hello/views.py
@@ -1,17 +1,26 @@
 # Create your views here.
 from django.views.generic import TemplateView, View
 from django.http import HttpResponse
-from .models import WebRequest
 
 import json
 
+from .models import Account, WebRequest
 
 class IndexPage(TemplateView):
     template_name = u'index.html'
 
     def get_context_data(self, **kwargs):
         context = super(IndexPage, self).get_context_data(**kwargs)
-
+        #context[u'account'] = self.request.user.account
+        context[u'account'] = {'first_name': 'My first name',
+                               'last_name': 'My last name',
+                               'birthday': 'date of my b',
+                               'email': 'email',
+                               'jabber': '22345',
+                               'skype': 'my_skype',
+                               'bio': 'bio',
+                               'other_info': 'other',
+                               }
         return context
 
 
